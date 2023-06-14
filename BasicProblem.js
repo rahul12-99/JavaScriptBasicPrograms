597ab2f31517d6fe08520639536f4891f87c4234
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-14 22:24:20 +0530

    Added : code for read single digit number and write in word

--- BasicProblem.js
+++ BasicProblem.js
@@ -1,8 +1,39 @@
-/*Write a program to simulate a coin flip and print out "Heads" or "Tails" accordingly. */
-let number = Math.random();
+/*Read a single digit number and write the number in word */
+const readline = require('readline');
 
-if (number < 0.5) {
-  console.log("HEAD");
-} else {
-  console.log("TAIL");
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Enter a single-digit number: ", function(number) {
+  let word = '';
+
+  if (number === '0') {
+    word = 'Zero';
+  } else if (number === '1') {
+    word = 'One';
+  } else if (number === '2') {
+    word = 'Two';
+  } else if (number === '3') {
+    word = 'Three';
+  } else if (number === '4') {
+    word = 'Four';
+  } else if (number === '5') {
+    word = 'Five';
+  } else if (number === '6') {
+    word = 'Six';
+  } else if (number === '7') {
+    word = 'Seven';
+  } else if (number === '8') {
+    word = 'Eight';
+  } else if (number === '9') {
+    word = 'Nine';
+  } else {
+    word = 'Invalid number';
+  }
+
+  console.log("Number in word : " + word);
+
+  rl.close();
+});
