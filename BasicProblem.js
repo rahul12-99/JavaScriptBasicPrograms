0282d5d1d464f17b3d35bcec4153bf82cfb4f45d
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-15 02:03:06 +0530

    Added : code for read number and display unit ten ..

--- BasicProblem.js
+++ BasicProblem.js
@@ -1,26 +1,12 @@
-/*Read a Number and Display the week day (Sunday, Monday,...)*/
-let number2 = prompt("Please Enter day number");
-if(number2==1){
-    console.log(" Day is Sunday");
-}
-else if(number2==2){
-    console.log("  Day is Monday");
-}
-else if(number2==3){
-    console.log(" Day is Tuesday");
-}
-else if(number2==4){
-    console.log(" Day is Wednesday");
-}
-else if(number2==5){
-    console.log(" Day is Thursday");
-}
-else if(number2==6){
-    console.log(" Day is Friday");
-}
-else if(number2==7){
-    console.log(" Day is Saturday");
-}
-else{
-    console.log("Invalid Day Number");
-}
+/*Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...*/
+let num = prompt("Enter number");
+
+const placeValue = (num, res = [], factor = 1) => {
+    if(num){
+       const val = (num % 10) * factor;
+       res.unshift(val);
+       return placeValue(Math.floor(num / 10), res, factor * 10);
+    };
+    return res;
+ };
+ console.log(placeValue(num));
