362ff69d46a4e9b9cae736e5b31c67cd1953aa7c
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-15 02:07:35 +0530

    Added : code for do the arithmetic operation

--- BasicProblem.js
+++ BasicProblem.js
@@ -1,12 +1,12 @@
-/*Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...*/
-let num = prompt("Enter number");
+/*Enter 3 Numbers do following arithmetic operation and find the one that
+is maximum and minimum*/
+let x = prompt("Enter first number");
+ 
+ let y = prompt("Enter second number");
+ 
+ let z = prompt("Enter third number");
 
-const placeValue = (num, res = [], factor = 1) => {
-    if(num){
-       const val = (num % 10) * factor;
-       res.unshift(val);
-       return placeValue(Math.floor(num / 10), res, factor * 10);
-    };
-    return res;
- };
- console.log(placeValue(num));
+ console.log(x+(y*z));
+ console.log((x%y)+z);
+ console.log(z+(x/y));
+ console.log((x*y)+z);
